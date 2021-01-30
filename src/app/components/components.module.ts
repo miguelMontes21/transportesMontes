aef3a37725f3af33a4ae21b976be0abc4a303cb7
MiguelMontes <miguel.montes@bicevida.cl>
2021-01-30 09:49:30 -0300

    Quienes somos

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -13,6 +13,7 @@ import { NucleoiconsComponent } from './nucleoicons/nucleoicons.component';
 import { ComponentsComponent } from './components.component';
 import { NotificationComponent } from './notification/notification.component';
 import { NgbdModalBasic } from './modal/modal.component';
+import { ComponentsQComponent } from './componentsQ.component';
 
 @NgModule({
     imports: [
@@ -30,6 +31,7 @@ import { NgbdModalBasic } from './modal/modal.component';
         TypographyComponent,
         NucleoiconsComponent,
         NotificationComponent,
+        ComponentsQComponent,
         NgbdModalBasic
     ],
     exports:[ ComponentsComponent ]
