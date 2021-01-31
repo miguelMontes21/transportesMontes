f0f4c1ad0631c9ecd46b819cf2c939e50320c512
MiguelMontes <miguel.montes@bicevida.cl>
2021-01-31 11:21:08 -0300

    d

--- src/app/components/componentsI.component.ts
+++ src/app/components/componentsI.component.ts
@@ -3,6 +3,8 @@ import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { NgbAccordionConfig } from '@ng-bootstrap/ng-bootstrap';
 import * as Rellax from 'rellax';
 declare var jQuery: any;
+import $ from "jquery";
+
 
 @Component({
     selector: 'app-components',
@@ -48,17 +50,17 @@ export class ComponentsIComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit() {
-        $(function(){
-            var	btn = $(".slider__btn");
-            
-            btn.on("click",function(){
-                $(".slider__item").first().clone().appendTo(".slider");
-                $(".slider__image").first().css({transform: "rotateX(-180deg)", opacity: 0});
-                setTimeout(function(){
-                    $(".slider__item").first().remove();
-                },200);
-            });
-        });
+$(function(){
+	var	btn = $(".slider__btn");
+	
+	btn.on("click",function(){
+		$(".slider__item").first().clone().appendTo(".slider");
+		$(".slider__image").first().css({transform: "rotateX(-180deg)", opacity: 0});
+		setTimeout(function(){
+			$(".slider__item").first().remove();
+		},200);
+	});
+});
 
 
       var rellaxHeader = new Rellax('.rellax-header');
